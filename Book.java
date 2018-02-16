91cb40a4284c849315031cbd12cb992718e65a5e
Unknown <nedvar17@student.aau.dk>
2018-02-16 16:27:26 +0100

    Book class now with functions

--- Book.java
+++ Book.java
@@ -5,32 +5,40 @@ public class Book {
 
     private String name;
     private int id;
-    private int stock;
+    private int stockAmount;
     private boolean inStock;
+    private int totalAmount;
 
-    Book(String name, int id, int stock) {
+    Book(String name, int id, int stockAmount, int totalAmount) {
         this.name = name;
         this.id = id;
-        this.stock = stock;
+        this.stockAmount = stockAmount;
+        this.totalAmount = totalAmount;
         
-        if (stock >= 1) {
+        if (stockAmount >= 1) {
             inStock = true;
         } else {
             inStock = false;
         }
     }
 
-    public static bookInStock(String name) {
-        return true;
+    public static boolean bookInStock(String name) {
+        return inStock;
     }
 
     public static boolean bookInStock(int id) {
-        return true;
+        return inStock;
     }
 
-    public bookInStock() {
-        
+    public boolean bookInStock() {
+        return = bookInStock(this.name);
     }
 
+    public void setTotalAmount(int num) {
+        if(num >= 1) {
+        this.totalAmount = num;
+        System.out.println(this.name + " has its total amount set to " + this.totalAmount);
+        }
+    }
     
 }
